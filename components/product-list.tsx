508386484644732dfcab11a27ec9eff7c8508db5
danielcaldeir <danielcaldeir@gmail.com>
2024-05-08 14:26:56 -0300

    Adicionando os componentes do Produto

---
+++ components/product-list.tsx
@@ -0,0 +1,45 @@
+import { db } from "@/lib/prisma";
+import ProductItem from "@/components/product-item";
+import { Prisma } from "@prisma/client";
+
+interface ProductListProps {
+    products: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    }>[];
+}
+
+const ProductList = async ({ products }: ProductListProps) => {
+
+// const ProductList = async () => {
+  // const products = await db.product.findMany({
+  //   where: {
+  //     discountPercentage: {
+  //       gt: 0,
+  //     },
+  //   },
+  //   take: 10,
+  //   include: {
+  //     restaurant: {
+  //       select: {
+  //         name: true,
+  //       },
+  //     },
+  //   },
+  // });
+
+    return (
+      <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    );
+};
+
+export default ProductList;
